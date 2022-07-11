aa710e3404bdc8ae599cb2f57202662559659bb5
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-07-11 11:41:06 +0300

    Initial Commit

---
+++ Basic request/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic request
+//
+//  Created by Даниил Кирьянчук on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
